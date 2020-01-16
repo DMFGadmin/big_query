746243228fe1d65f4bcb2ccdc4bec964d56a049c
joel <joelcote68@gmail.com>
2020-01-16 12:00:37 -0600

    testing

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+module "big_query" {
+  source          = "./modules/big_query"
 
+  dataset_id = var.dataset_id
+  dataset_name = var.dataset_name
+  description = var.description
+  project_id = var.project_id
+}
